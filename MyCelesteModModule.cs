241c6871d3a35a5dd7d66376eb3ad05a558fae8a
Saplonily <Saplonily@outlook.com>
2023-07-08 13:35:55 +0800

    init

---
+++ MyCelesteModModule.cs
@@ -0,0 +1,14 @@
+﻿namespace MyCelesteMod;
+
+public class MyCelesteModModule : EverestModule
+{
+    public override void Load()
+    {
+        Logger.Log(LogLevel.Info, "MyCelesteMod", "Hello World!");
+        
+    }
+
+    public override void Unload()
+    {
+    }
+}
