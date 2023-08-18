3b09bbb640055fc9193d912a4efd55059eac2eba
Saplonily <Saplonily@outlook.com>
2023-08-18 23:22:26 +0800

    even more update (with core versions!)

--- MyCelesteModModule.cs
+++ MyCelesteModModule.cs
@@ -1,13 +1,14 @@
-﻿namespace MyCelesteMod;
+namespace MyCelesteMod;
 
 public class MyAwesomeModModule : EverestModule
 {
     public override void Load()
     {
-        Logger.Log(LogLevel.Info, "MyCelesteMod", "Hello World!");
+        
     }
 
     public override void Unload()
     {
+        
     }
 }
