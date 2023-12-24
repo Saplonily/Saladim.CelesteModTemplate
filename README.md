37c8db94e2ad121b9bc50be2b84622a6df2447b5
Saplonily <Saplonily@outlook.com>
2023-12-24 09:51:27 +0800

    sorting

---
+++ README.md
@@ -0,0 +1,3 @@
+# Saladim.CelesteModTemplate
+
+Another lightweight Celeste Mod Template! Used by [CelesteModTotorial (sadly chinese only)](https://celestemod.saplonily.link)!
--- content/MyCelesteMod/MyCelesteModModule.cs
+++ content/MyCelesteMod/MyCelesteModModule.cs
@@ -1,13 +1,13 @@
-namespace MyCelesteMod;
+namespace Celeste.Mod.MyCelesteMod;
 
 public class MyCelesteModModule : EverestModule
 {
     public override void Load()
     {
+        Instance = this;
     }
 
     public override void Unload()
     {
-        
     }
 }
